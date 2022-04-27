9addc13362532b4922d2191fc0ae36b0b88ba5c1
GetMyIsland <92424965+getmyisland@users.noreply.github.com>
2022-04-27 10:19:17 +0200

    Delete src directory

--- src/com/getmyisland/gui/Pinboard.java
+++
@@ -1,5 +0,0 @@
-package com.getmyisland.gui;
-
-public class Pinboard {
-
-}
--- src/com/getmyisland/main/Main.java
+++
@@ -1,8 +0,0 @@
-package com.getmyisland.main;
-
-public class Main {
-
-    public static void main(String[] args) {
-        
-    }
-}
