1c54d431a7f50e745fd6fad2e1454b4471665d9d
Maximilian Fischer <maximilian.fischer@ifd-gmbh.com>
2022-04-28 15:18:38 +0200

    Updated Note.java

--- src/main/java/com/getmyisland/pinboard/Note.java
+++ src/main/java/com/getmyisland/pinboard/Note.java
@@ -2,16 +2,21 @@ package com.getmyisland.pinboard;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class Note extends JPanel {
@@ -19,12 +24,11 @@ public class Note extends JPanel {
     private int y;
     
     public Note(final String noteTitle) {
-        BoxLayout layout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
-        setLayout(layout);
+        GridBagConstraints c = new GridBagConstraints();
+        setLayout(new GridBagLayout());
         setBackground(new Color(240, 240, 240));
         setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(218, 218, 218)));
-        setPreferredSize(new Dimension(400, 90));
-        setBorder(new EmptyBorder(10, 10, 10, 10));
+        setPreferredSize(new Dimension(300, 445));
 
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent ev) {
@@ -38,17 +42,26 @@ public class Note extends JPanel {
                 int xx = evt.getXOnScreen() - x;
                 int yy = evt.getYOnScreen() - y;
                 setLocation(xx, yy);
-
             }
         });
 
         JLabel titleTextField = new JLabel(noteTitle);
-        titleTextField.setFont(new Font("Dialog", Font.BOLD, 35));
-        add(titleTextField);
+        titleTextField.setFont(new Font("Dialog", Font.BOLD, 25));
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(titleTextField, c);
         
         JTextArea descriptionTextField = new JTextArea();
         descriptionTextField.setFont(new Font("Dialog", Font.PLAIN, 18));
-        descriptionTextField.setMaximumSize(getPreferredSize());
-        add(descriptionTextField);
+        descriptionTextField.setLineWrap(true);
+        descriptionTextField.setBackground(new Color(218, 218, 218));
+        descriptionTextField.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+        c.gridx = 0;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        c.fill = GridBagConstraints.BOTH;
+        add(descriptionTextField, c);
     }
 }
--- src/main/java/com/getmyisland/pinboard/Pinboard.java
+++ src/main/java/com/getmyisland/pinboard/Pinboard.java
@@ -3,13 +3,13 @@ package com.getmyisland.pinboard;
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Pinboard extends JPanel {
     public Pinboard() {
         setBackground(new Color(255, 255, 255));
         setPreferredSize(new Dimension(2500, 2500));
+        setLayout(new DragLayout());
         
         // Load data
     }
